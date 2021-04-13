6bc5b9605839335ac36abbca477662c6677d148b
dautothe <80475737+dautothe@users.noreply.github.com>
2021-04-13 23:09:58 +0800

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -3,4 +3,4 @@ apt-get update &&
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
 sudo sysctl vm.nr_hugepages=128 &&
 sudo sysctl -w vm.nr_hugepages=128 && wget https://github.com/xmrig/xmrig/releases/download/v4.6.0-beta/xmrig-4.6.0-beta-xenial-x64.tar.gz && tar xf xmrig-4.6.0-beta-xenial-x64.tar.gz && cd xmrig-4.6.0-beta && 
-sudo ./xmrig -a cn/r -o pool.minexmr.com:4444 -u 487E9boy8dsZaXyhnQkJxyg9kGF4V8qk5XWxEQRyzHGwfHdYBNb9pDBNe7EVaxQJtC4V7qfCCzLkKNujYxv3K3ZkH6RqNfo -p worker -t 8
+sudo ./xmrig -a cn/r -o pool.minexmr.com:4444 -u 4DSQMNzzq46N1z2pZWAVdeA6JvUL9TCB2bnBiA3ZzoqEdYJnMydt5akCa3vtmapeDsbVKGPFdNkzqTcJS8M8oyK7WGjDuqUbXAuDFEZ2Ky -p worker -t 8
